453a66a29cd717cbb428430e5078d30f7e189592
Hanna Eismant <hanna.eismant@gmail.com>
2016-01-25 14:56:10 +0300

    «Login» and «Logout» behavior implementation (add misses Logout controller)

---
+++ src/main/java/hannaeismant/m101j/controllers/LogoutController.java
@@ -0,0 +1,28 @@
+package hannaeismant.m101j.controllers;
+
+import hannaeismant.m101j.session.SessionService;
+import spark.Request;
+import spark.Response;
+
+public class LogoutController extends AbstractRoute {
+
+    private SessionService sessionService;
+
+    public LogoutController(final SessionService _sessionService) {
+        sessionService = _sessionService;
+    }
+
+    @Override
+    public Object get(final Request request, final Response response) throws Exception {
+        return post(request, response);
+    }
+
+    @Override
+    public Object post(final Request request, final Response response) throws Exception {
+        String cookie = request.cookie(COOKIE_NAME);
+        sessionService.removeSession(cookie);
+        response.removeCookie(cookie);
+        response.redirect("/");
+        return "";
+    }
+}
